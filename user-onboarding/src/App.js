5690c57227057fcdbe57206851e5fc7b6d1355e2
Iaroslav Mokroguz <maustrauk@gmail.com>
2020-11-11 14:48:39 -0500

    Task_2 Create Your Advanced Form

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,12 +1,23 @@
 import axios from 'axios';
-import React from 'react';
+import React, {useState} from 'react';
 import * as yup from 'yup';
 
 import './App.css';
 import Form from './Form';
 
+const initialFormValues = {
+  name: "",
+  email: "",
+  password: "",
+  terms: "",
+}
+
 
 function App() {
+
+  const [formValues, setFormValues] = useState(initialFormValues); 
+
+
   return (
     <div className="App">
       <Form />
--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -1,10 +1,52 @@
 import React from 'react';
 
 const Form = props => {
+
+    const onChange = evt => {
+
+    }
     
     return(
         <div className="form">
-
+            <form>
+                <label>
+                    Name : 
+                    <input
+                    name = "name"
+                    type = "text"
+                    value = "name"
+                    onChange ={onChange}
+                    />
+                </label>
+                <label>
+                    Email : 
+                    <input
+                    name = "email"
+                    type = "email"
+                    value = "email"
+                    onChange ={onChange}
+                    />
+                </label>
+                <label>
+                    Password : 
+                    <input
+                    name = "password"
+                    type = "text"
+                    value = "password"
+                    onChange ={onChange}
+                    />
+                </label>
+                <label>
+                    Terms of Service
+                    <input
+                    name = "terms"
+                    type = "checkbox"
+                    value = "on"
+                    onChange ={onChange}
+                    />
+                </label>
+                <button>submit</button>
+            </form>
         </div>
     );
 }
