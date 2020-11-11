6bd1ea8c7a4db81b77d23783e1f3b0584798a056
Iaroslav Mokroguz <maustrauk@gmail.com>
2020-11-11 14:25:42 -0500

    Task_1 Form.js

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -3,11 +3,13 @@ import React from 'react';
 import * as yup from 'yup';
 
 import './App.css';
+import Form from './Form';
 
 
 function App() {
   return (
     <div className="App">
+      <Form />
     </div>
   );
 }
